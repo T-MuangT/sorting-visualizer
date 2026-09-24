2c3918fd0cc7c2c9d1c28523b65b67c3bc437742
T-MuangT <teerapath.muangjan@g.swu.ac.th>
2026-09-24 22:22:08 +0700

    Mark build script executable

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -euo pipefail
+set -eu
 
 BUILD_DIR="${1:-build}"
 INSTALL_PREFIX="${2:-install}"
 
-cmake -S .
--B "$BUILD_DIR"
--G Ninja
--DCMAKE_BUILD_TYPE=Release
--DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
+cmake -S . \
+    -B "$BUILD_DIR" \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
 
 cmake --build "$BUILD_DIR" --parallel
 
